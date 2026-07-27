5f78eba777ff3b71cad0f6c6121dc2b8476cc577
Abdallah <abdallahhegazy98@gmail.com>
2026-07-27 17:19:16 +0300

    add testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
+import sys
+from pathlib import Path
+
+# Add project root to Python path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from app import app
+
 
 def test_home():
     client = app.test_client()
@@ -6,3 +13,11 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
+
+
+def test_home_returns_json():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.is_json
